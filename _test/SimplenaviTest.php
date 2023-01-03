d8ce5486268fd1c02234429a87c5de7aaa6425c7
Andreas Gohr <andi@splitbrain.org>
2023-01-03 09:37:23 +0100

    code cleanup

---
+++ _test/SimplenaviTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace dokuwiki\plugin\simplenavi\test;
+
+use DokuWikiTest;
+use TestRequest;
+
+/**
+ * General tests for the simplenavi plugin
+ *
+ * @author  Michael Große <grosse@cosmocode.de>
+ *
+ * @group plugin_simplenavi
+ * @group plugins
+ */
+class SimplenaviTest extends DokuWikiTest
+{
+
+    protected $pluginsEnabled = array('simplenavi');
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        saveWikiText('sidebar', '{{simplenavi>}}', 'create test sidebar');
+        saveWikiText('namespace1:foo', 'bar', 'foobar');
+        saveWikiText('namespace2:foo', 'bar', 'foobar');
+        saveWikiText('namespace12:foo', 'bar', 'foobar');
+        saveWikiText('namespace123:foo', 'bar', 'foobar');
+        saveWikiText('namespace21:foo', 'bar', 'foobar');
+
+    }
+
+    /**
+     * @covers syntax_plugin_simplenavi
+     */
+    public function testOutputNatural()
+    {
+        global $ID, $conf;
+        $conf['plugin']['simplenavi']['sort'] = 'natural';
+
+        $ID = 'wiki:start';
+        $request = new TestRequest();
+        $input = [
+            'id' => 'namespace1:foo',
+        ];
+        saveWikiText('wiki:start', 'some text', 'Test init');
+        $response = $request->post($input);
+        $naviBegin = strpos($response->getContent(), '<!-- ********** ASIDE ********** -->') + 36;
+        $naviEnd = strpos($response->getContent(), '<!-- /aside -->');
+        $navi = substr($response->getContent(), $naviBegin, $naviEnd - $naviBegin);
+        $navilines = explode("\n", $navi);
+        $listlines = [];
+        foreach ($navilines as $line) {
+            if (substr($line, 0, 4) != '<li ') continue;
+            if (strpos($line, 'namespace') === false) continue;
+            $listlines[] = $line;
+        }
+
+        $this->assertTrue(
+            strpos($listlines[0], 'href="/./doku.php?id=namespace1:start"') !== false,
+            'namespace1 should be before other namespaces and espacially before its subpages and namespaces'
+        );
+        $this->assertTrue(
+            strpos($listlines[1], 'href="/./doku.php?id=namespace1:foo"') !== false,
+            'level2 should follow open level1'
+        );
+        $this->assertTrue(
+            strpos($listlines[2], 'href="/./doku.php?id=namespace2:start"') !== false,
+            'namespace2 should be after namespace1 and its pages.'
+        );
+        $this->assertTrue(
+            strpos($listlines[3], 'href="/./doku.php?id=namespace12:start"') !== false,
+            'namespace12 should be after namespace2.'
+        );
+        $this->assertTrue(
+            strpos($listlines[4], 'href="/./doku.php?id=namespace21:start"') !== false,
+            'namespace21 should be after namespace12.'
+        );
+        $this->assertTrue(
+            strpos($listlines[5], 'href="/./doku.php?id=namespace123:start"') !== false,
+            'namespace123 should be after namespace21.'
+        );
+    }
+
+    /**
+     * @covers syntax_plugin_simplenavi
+     */
+    public function testOutputAscii()
+    {
+        global $ID, $conf;
+        $conf['plugin']['simplenavi']['sort'] = 'ascii';
+
+        $ID = 'wiki:start';
+        $request = new TestRequest();
+        $input = [
+            'id' => 'namespace1:foo',
+        ];
+        saveWikiText('wiki:start', 'some text', 'Test init');
+        $response = $request->post($input);
+        $naviBegin = strpos($response->getContent(), '<!-- ********** ASIDE ********** -->') + 36;
+        $naviEnd = strpos($response->getContent(), '<!-- /aside -->');
+        $navi = substr($response->getContent(), $naviBegin, $naviEnd - $naviBegin);
+        $navilines = explode("\n", $navi);
+        $listlines = [];
+        foreach ($navilines as $line) {
+            if (substr($line, 0, 4) != '<li ') continue;
+            if (strpos($line, 'namespace') === false) continue;
+            $listlines[] = $line;
+        }
+
+        $this->assertTrue(
+            strpos($listlines[0], 'href="/./doku.php?id=namespace1:start"') !== false,
+            'namespace1 should be before other namespaces and espacially before its subpages and namespaces'
+        );
+        $this->assertTrue(
+            strpos($listlines[1], 'href="/./doku.php?id=namespace1:foo"') !== false,
+            'level2 should follow open level1.'
+        );
+        $this->assertTrue(
+            strpos($listlines[2], 'href="/./doku.php?id=namespace12:start"') !== false,
+            'namespace12 should be after namespace1 and its pages.'
+        );
+        $this->assertTrue(
+            strpos($listlines[3], 'href="/./doku.php?id=namespace123:start"') !== false,
+            'namespace123 should be after namespace12.'
+        );
+        $this->assertTrue(strpos(
+                $listlines[4], 'href="/./doku.php?id=namespace2:start"') !== false,
+            'namespace2 should be after namespace123.'
+        );
+        $this->assertTrue(
+            strpos($listlines[5], 'href="/./doku.php?id=namespace21:start"') !== false,
+            'namespace21 should be after namespace2.'
+        );
+    }
+
+}
+
+
+
--- syntax.php
+++ syntax.php
@@ -1,4 +1,7 @@
 <?php
+
+use dokuwiki\File\PageResolver;
+
 /**
  * DokuWiki Plugin simplenavi (Syntax Component)
  *
@@ -6,178 +9,232 @@
  * @author  Andreas Gohr <gohr@cosmocode.de>
  */
 
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
+class syntax_plugin_simplenavi extends DokuWiki_Syntax_Plugin
+{
+    private $startpages = [];
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'syntax.php';
-require_once DOKU_INC.'inc/search.php';
-
-class syntax_plugin_simplenavi extends DokuWiki_Syntax_Plugin {
-    function getType() {
+    /** @inheritdoc */
+    public function getType()
+    {
         return 'substition';
     }
 
-    function getPType() {
+    /** @inheritdoc */
+    public function getPType()
+    {
         return 'block';
     }
 
-    function getSort() {
+    /** @inheritdoc */
+    public function getSort()
+    {
         return 155;
     }
 
-
-    function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('{{simplenavi>[^}]*}}',$mode,'plugin_simplenavi');
+    /** @inheritdoc */
+    public function connectTo($mode)
+    {
+        $this->Lexer->addSpecialPattern('{{simplenavi>[^}]*}}', $mode, 'plugin_simplenavi');
     }
 
-    function handle($match, $state, $pos, Doku_Handler $handler){
-        $data = array(cleanID(substr($match,13,-2)));
+    /** @inheritdoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler)
+    {
+        $data = [cleanID(substr($match, 13, -2))];
 
         return $data;
     }
 
-    function render($mode, Doku_Renderer $R, $pass) {
-        if($mode != 'xhtml') return false;
+    /** @inheritdoc */
+    public function render($format, Doku_Renderer $renderer, $data)
+    {
+        if ($format != 'xhtml') return false;
 
         global $conf;
         global $INFO;
-        $R->info['cache'] = false;
+        $renderer->info['cache'] = false;
 
-        $ns = utf8_encodeFN(str_replace(':','/',$pass[0]));
-        $data = array();
-        search($data,$conf['datadir'],array($this,'_search'),array('ns' => $INFO['id']),$ns,1,'natural');
-        if ($this->getConf('sortByTitle') == true) {
-            $this->_sortByTitle($data,"id");
+        $ns = utf8_encodeFN(str_replace(':', '/', $data[0]));
+        $items = [];
+        search($items, $conf['datadir'], [$this, 'cbSearch'], ['ns' => $INFO['id']], $ns, 1, 'natural');
+        if ($this->getConf('sortByTitle')) {
+            $this->sortByTitle($items, "id");
         } else {
             if ($this->getConf('sort') == 'ascii') {
-                uksort($data, array($this, '_cmp'));
+                uksort($items, [$this, 'pathCompare']);
             }
         }
 
-        $R->doc .= '<div class="plugin__simplenavi">';
-        $R->doc .= html_buildlist($data,'idx',array($this,'_list'),array($this,'_li'));
-        $R->doc .= '</div>';
+        $renderer->doc .= '<div class="plugin__simplenavi">';
+        $renderer->doc .= html_buildlist($items, 'idx', [$this, 'cbList'], [$this, 'cbListItem']);
+        $renderer->doc .= '</div>';
 
         return true;
     }
 
-    function _list($item){
+    /**
+     * Create a list openening
+     *
+     * @param array $item
+     * @return string
+     * @see html_buildlist()
+     */
+    public function cbList($item)
+    {
         global $INFO;
 
-        if(($item['type'] == 'd' && $item['open']) || $INFO['id'] == $item['id']){
-            return '<strong>'.html_wikilink(':'.$item['id'],$this->_title($item['id'])).'</strong>';
-        }else{
-            return html_wikilink(':'.$item['id'],$this->_title($item['id']));
+        if (($item['type'] == 'd' && $item['open']) || $INFO['id'] == $item['id']) {
+            return '<strong>' . html_wikilink(':' . $item['id'], $this->getTitle($item['id'])) . '</strong>';
+        } else {
+            return html_wikilink(':' . $item['id'], $this->getTitle($item['id']));
         }
 
     }
 
-    function _li($item){
-        if($item['type'] == "f"){
-            return '<li class="level'.$item['level'].'">';
-        }elseif($item['open']){
+    /**
+     * Create a list item
+     *
+     * @param array $item
+     * @return string
+     * @see html_buildlist()
+     */
+    public function cbListItem($item)
+    {
+        if ($item['type'] == "f") {
+            return '<li class="level' . $item['level'] . '">';
+        } elseif ($item['open']) {
             return '<li class="open">';
-        }else{
+        } else {
             return '<li class="closed">';
         }
     }
 
-    function _search(&$data,$base,$file,$type,$lvl,$opts){
+    /**
+     * Custom search callback
+     *
+     * @param $data
+     * @param $base
+     * @param $file
+     * @param $type
+     * @param $lvl
+     * @param $opts
+     * @return bool
+     */
+    public function cbSearch(&$data, $base, $file, $type, $lvl, $opts)
+    {
         global $conf;
         $return = true;
 
-        $item = array();
-
         $id = pathID($file);
 
-        if($type == 'd' && !(
-            preg_match('#^'.$id.'(:|$)#',$opts['ns']) ||
-            preg_match('#^'.$id.'(:|$)#',getNS($opts['ns']))
+        if ($type == 'd' && !(
+                preg_match('#^' . $id . '(:|$)#', $opts['ns']) ||
+                preg_match('#^' . $id . '(:|$)#', getNS($opts['ns']))
 
-        )){
+            )) {
             //add but don't recurse
             $return = false;
-        }elseif($type == 'f' && (!empty($opts['nofiles']) || substr($file,-4) != '.txt')){
+        } elseif ($type == 'f' && (!empty($opts['nofiles']) || substr($file, -4) != '.txt')) {
             //don't add
             return false;
         }
 
-        if($type=='d' && $conf['sneaky_index'] && auth_quickaclcheck($id.':') < AUTH_READ){
+        if ($type == 'd' && $conf['sneaky_index'] && auth_quickaclcheck($id . ':') < AUTH_READ) {
             return false;
         }
 
-        if($type == 'd'){
+        if ($type == 'd') {
             // link directories to their start pages
-            $exists = false;
             $id = "$id:";
-            resolve_pageid('',$id,$exists);
+            $id = (new PageResolver(''))->resolveId($id);
             $this->startpages[$id] = 1;
-        }elseif(!empty($this->startpages[$id])){
+        } elseif (!empty($this->startpages[$id])) {
             // skip already shown start pages
             return false;
-        }elseif(noNS($id) == $conf['start']){
+        } elseif (noNS($id) == $conf['start']) {
             // skip the main start page
             return false;
         }
 
         //check hidden
-        if(isHiddenPage($id)){
+        if (isHiddenPage($id)) {
             return false;
         }
 
         //check ACL
-        if($type=='f' && auth_quickaclcheck($id) < AUTH_READ){
+        if ($type == 'f' && auth_quickaclcheck($id) < AUTH_READ) {
             return false;
         }
 
-        $data[$id]=array( 'id'    => $id,
-                       'type'  => $type,
-                       'level' => $lvl,
-                       'open'  => $return);
+        $data[$id] = array(
+            'id' => $id,
+            'type' => $type,
+            'level' => $lvl,
+            'open' => $return,
+        );
         return $return;
     }
 
-    function _title($id) {
+    /**
+     * Get the title for the given page ID
+     *
+     * @param string $id
+     * @return string
+     */
+    protected function getTitle($id)
+    {
         global $conf;
 
-        if(useHeading('navigation')){
+        if (useHeading('navigation')) {
             $p = p_get_first_heading($id);
         }
-        if(!empty($p)) return $p;
+        if (!empty($p)) return $p;
 
         $p = noNS($id);
-        if ($p == $conf['start'] || $p == false) {
+        if ($p == $conf['start'] || !$p) {
             $p = noNS(getNS($id));
-            if ($p == false) {
+            if (!$p) {
                 return $conf['start'];
             }
         }
         return $p;
     }
 
-    function _cmp($a, $b) {
+    /**
+     * Custom comparator to compare IDs
+     *
+     * @param string $a
+     * @param string $b
+     * @return int
+     */
+    public function pathCompare($a, $b)
+    {
         global $conf;
-        $a = preg_replace('/'.preg_quote($conf['start'], '/').'$/', '', $a);
-        $b = preg_replace('/'.preg_quote($conf['start'], '/').'$/', '', $b);
+        $a = preg_replace('/' . preg_quote($conf['start'], '/') . '$/', '', $a);
+        $b = preg_replace('/' . preg_quote($conf['start'], '/') . '$/', '', $b);
         $a = str_replace(':', '/', $a);
         $b = str_replace(':', '/', $b);
 
         return strcmp($a, $b);
     }
 
-    function _sortByTitle(&$array, $key) {
-        $sorter = array();
-        $ret = array();
+    /**
+     * Sort items by title
+     *
+     * @param array[] $array a list of items
+     * @param string $key the key that contains the page ID in each item
+     * @return void
+     */
+    protected function sortByTitle(&$array, $key)
+    {
+        $sorter = [];
+        $ret = [];
         reset($array);
         foreach ($array as $ii => $va) {
-            $sorter[$ii] = $this->_title($va[$key]);
+            $sorter[$ii] = $this->getTitle($va[$key]);
         }
         if ($this->getConf('sort') == 'ascii') {
-            uksort($sorter, array($this, '_cmp'));
+            uksort($sorter, [$this, 'pathCompare']);
         } else {
             natcasesort($sorter);
         }
@@ -188,5 +245,3 @@ class syntax_plugin_simplenavi extends DokuWiki_Syntax_Plugin {
     }
 
 }
-
-// vim:ts=4:sw=4:et:
